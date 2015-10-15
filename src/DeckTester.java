8ba3a75ddd294030b88438f321156370f983f375
Gilmour <horriganb@gilmour.org>
2015-10-15 15:23:56 -0400

    Activity 2

---
+++ src/DeckTester.java
@@ -0,0 +1,16 @@
+
+public class DeckTester {
+	/**
+	 * This is a class that tests the Deck class.
+	 */
+
+		/**
+		 * The main method in this class checks the Deck operations for consistency.
+		 *	@param args is not used.
+		 */
+		public static void main(String[] args) {
+			/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		}
+	
+
+}
